e058af984206482a4c1570840d294102887a464b
Tony Jing <tonyxjing@gmail.com>
2014-09-19 10:58:38 -0700

    Refactored code to include a seperate CSS file, added local images and various icons

--- js/modify.js
+++ js/modify.js
@@ -1,5 +1,30 @@
+
+// dxwang -- Please replace these placeholder values with dynamically udpated ones from Netflix and other sources
+
+var movieTitle = "This is One of the Movie Titles";
+var directedBy = "Iamadirector Name";
+var firstActor = "Iamthe Firstactor";
+var secondActor = "Iamthe Secondactor";
+var starring = firstActor + ", " + secondActor;
+var yearProduced = "2001";
+
+var numOscars = 2;
+var numGoldens = 2;
+var numEmmys = 2;
+
+var imdbRating = "79%";
+var rottenCriticRating = "72%";
+var rottenAudienceRating = "90%";
+var metaCriticRating = "62";
+var metaUserRating = "9.4";
+
+
+
+// Wrapping code in a self-executing function below to enable the private use of the $
+
 (function($){
 	$('.connect-overlay.connect').remove();
+
 	$('#hd').css({
 		height: '75px',
 		marginTop: "-12px",
@@ -17,84 +42,20 @@
 		float: 'right',
 	});
 
-	$('<div class="added-description"><div class="nested-div"></div></div>').appendTo('body').css({
-		position: "fixed",
-		width: "32%",
-		top: 0,
-		bottom: 0,
-		left: 0,
-		backgroundImage: "url('http://upload.wikimedia.org/wikipedia/commons/3/30/Cinemaaustralia.jpg')",
-		backgroundSize: "cover"
-	});
-
-	$('.nested-div').css({
-		width: "100%",
-		height: "100%",	
-		position: "relative",
-		backgroundColor: "rgba(0,0,0, 0.75)"
-	}).append('<h2 class="opening-title">What would you like to watch today?</h2>');
+	$('<div class="added-description"><div class="nested-div"><h2 class="opening-title">What would you like to watch today?</h2></div></div>').appendTo('body');
 
 	var openTitle = $('.opening-title');
 
-	openTitle.css({
-		position: "absolute",
-		top: "36%",
-		color: "rgba(255,255,255, 0.9)",
-		fontWeight: "300",
-		fontSize: "55px",
-		margin: "0 70px",
-		fontFamily: "Didot,serif"
-	});
-
-	var movieTitle = "This is One of the Movie Titles";
-	var directedBy = "Iamadirector Name";
-	var firstActor = "Iamthe Firstactor";
-	var secondActor = "Iamthe Secondactor";
-	var starring = firstActor + ", " + secondActor;
-	var yearProduced = "2001";
-
-
-
 	$('.boxShot').on('hover', function(){
 		openTitle.hide();
+
 		var img = $(this).find('.boxShotImg').clone().addClass("img-injected");
 		$(".nested-div").find(".img-injected, .info-blob, .ratings").remove();
 
 		img.appendTo($(".nested-div"));
-
 		$(".nested-div").append("<div class='info-blob'><div class='movie-title'>" + movieTitle + "</div><div class='directed-by'>Director: " + directedBy + "</div><div class='starring'>Starring: " + starring + "</div><div class='year-produced'>" + yearProduced + "</div></div>");
-
-		$('.img-injected').css({
-			marginTop: "100px",
-			marginLeft: "30px",
-			width: "45%",
-			float: "left"
-		});
-
-		$('.info-blob').css({
-			float: "left",
-			width: "40%",
-			marginTop: "100px",
-			marginLeft: "4%",
-			color: "rgba(245,245,245, 0.9)",
-			marginRight: "3%"
-		});
-
-		$('.movie-title').css({
-			fontSize: "22px",
-			marginBottom: "20px",
-			fontWeight: "600"
-		});
-
-		$('.directed-by, .starring, .year-produced').css({
-			marginBottom: "11px"
-		});
-
-		$('<div class="ratings">Ratings<div class="imdb-rating"><span class="imdb-number"></span></div><div class="rt-rating"><span class="rt-number"></span></div><div class="mc-rating"><span class="rt-number"></span></div></div>').appendTo('.nested-div').css({
-			clear: "both",
-			position: 'absolute',
-			top: "420px",
-			left: "30px"
-		});
+		$('<div class="ratings"><div class="imdb-rating"><span class="rate imdb-number"></span></div><div class="rt-rating"><span class="rate rt-number"></span></div><div class="mc-rating"><span class="rate mc-number"></span></div></div>').appendTo('.nested-div');
 	});
+
+
 })(jQuery);
