0d134ebb4b0ede06ce083aa548d4842b0d4fa336
Tony Jing <tonyxjing@gmail.com>
2014-09-20 14:36:15 -0700

    committing new css styles and refactoring of js

--- js/modify.js
+++ js/modify.js
@@ -12,11 +12,12 @@ var numOscars = 2;
 var numGoldens = 2;
 var numEmmys = 2;
 
-var imdbRating = "79%";
-var rottenCriticRating = "72%";
-var rottenAudienceRating = "90%";
+var imdbRating = "79";
+var rottenCriticRating = "72";
+var rottenAudienceRating = "90";
 var metaCriticRating = "62";
-var metaUserRating = "9.4";
+var metaUserRating = "94";
+var infoDescription = "Lorem Ipsum movie description details";
 
 
 
@@ -24,23 +25,6 @@ var metaUserRating = "9.4";
 
 (function($){
 	$('.connect-overlay.connect').remove();
-
-	$('#hd').css({
-		height: '75px',
-		marginTop: "-12px",
-		zIndex: 100
-	});
-
-	$('#footer').css({
-		clear: "both",
-		float: "right",
-		width: "65%"
-	});
-
-	$('.main-content').css({
-		width: '65%',
-		float: 'right',
-	});
 
 	$('<div class="added-description"><div class="nested-div"><h2 class="opening-title">What would you like to watch today?</h2></div></div>').appendTo('body');
 
@@ -53,9 +37,8 @@ var metaUserRating = "9.4";
 		$(".nested-div").find(".img-injected, .info-blob, .ratings").remove();
 
 		img.appendTo($(".nested-div"));
-		$(".nested-div").append("<div class='info-blob'><div class='movie-title'>" + movieTitle + "</div><div class='directed-by'>Director: " + directedBy + "</div><div class='starring'>Starring: " + starring + "</div><div class='year-produced'>" + yearProduced + "</div></div>");
-		$('<div class="ratings"><div class="imdb-rating"><span class="rate imdb-number"></span></div><div class="rt-rating"><span class="rate rt-number"></span></div><div class="mc-rating"><span class="rate mc-number"></span></div></div>').appendTo('.nested-div');
+		$(".nested-div").append('<div class="ratings"><div class="imdb-rating"><span class="rate imdb-number">'+ imdbRating +'</span></div><div class="rt-rating"><span class="rate rt-number"><span class="user-base">Critics:</span>'+ rottenCriticRating +'<span class="user-base">Users:</span><span class="second-rating">'+ rottenAudienceRating +'</span></span></div><div class="mc-rating"><span class="rate mc-number"><span class="user-base">Critics:</span> '+ metaCriticRating +'<span class="user-base">User:</span><span class="second-rating">'+ metaUserRating +'</span></span></div></div>');
+		$("<div class='info-blob'><div class='movie-title'>" + movieTitle + "</div><div class='directed-by'>Director: " + directedBy + "</div><div class='starring'>Starring: " + starring + "</div><div class='year-produced'>" + yearProduced + "</div><div class='info-description'>" + infoDescription + "</div></div>").appendTo('.nested-div');
+
 	});
-
-
 })(jQuery);
